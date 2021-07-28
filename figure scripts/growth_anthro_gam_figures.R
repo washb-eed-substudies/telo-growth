ac98cf5ac939dadabae7692012e60c6a50b355e5
Andrew Mertens <amertens@berkeley.edu>
2021-07-28 11:13:35 -0700

    manuscript updates

---
+++ figure scripts/growth_anthro_gam_figures.R
@@ -0,0 +1,114 @@
+
+
+
+
+
+rm(list=ls())
+#renv::deactivate()
+source(here::here("0-config.R"))
+
+H1_adj_res <- readRDS(here("results/gam_results/adjusted/telot2_adj_res.RDS")) %>% mutate(H=1)
+H2_adj_res <- readRDS(here("results/gam_results/adjusted/telot3_adj_res.RDS")) %>% mutate(H=2)
+H3_adj_res <- readRDS(here("results/gam_results/adjusted/dtelo_adj_res.RDS")) %>% mutate(H=3)
+
+d <- bind_rows(H1_adj_res, H2_adj_res, H3_adj_res) %>% rename(A=X)
+head(d)
+
+unique(d$Y)
+unique(d$A)
+
+d$Y <- as.character(d$Y)
+d$Y <- gsub("whz", "wlz", d$Y)
+d$Y <- factor(d$Y, levels=c("laz_t2", "wlz_t2", "waz_t2", "hcz_t2",
+                            "laz_t3", "wlz_t3", "waz_t3", "hcz_t3",
+                            "delta_laz_t2_t3", "delta_wlz_t2_t3", "delta_waz_t2_t3", "delta_hcz_t2_t3",    
+                            "len_velocity_t2_t3", "wei_velocity_t2_t3", "hc_velocity_t2_t3"))
+
+d <- d %>% arrange(Y) %>%
+  mutate(
+    Ylab = case_when(
+      Y=="delta_laz_t2_t3" ~ "Change in LAZ\nbetween years 1 and 2",
+      Y=="delta_wlz_t2_t3" ~ "Change in WLZ\nbetween years 1 and 2",
+      Y=="delta_waz_t2_t3" ~ "Change in WAZ\nbetween years 1 and 2",
+      Y=="delta_hcz_t2_t3" ~ "Change in HCZ\nbetween years 1 and 2",
+      Y=="len_velocity_t2_t3" ~ "Length velocity (cm/month)\nbetween years 1 and 2",
+      Y=="wei_velocity_t2_t3" ~ "Weight velocity (kg/month)\nbetween years 1 and 2",
+      Y=="hc_velocity_t2_t3" ~ "Head circumference velocity (cm/month)\nbetween years 1 and 2",
+      Y=="laz_t2" ~ "LAZ at year 1",
+      Y=="wlz_t2" ~ "WLZ at year 1",
+      Y=="waz_t2" ~ "WAZ at year 1",
+      Y=="hcz_t2" ~ "HCZ at year 1",
+      Y=="laz_t3" ~ "LAZ at year 2",
+      Y=="wlz_t3" ~ "WLZ at year 2",
+      Y=="waz_t3" ~ "WAZ at year 2",
+      Y=="hcz_t3" ~ "HCZ at year 2"),
+    Ylab = factor(Ylab, levels=unique(Ylab)),
+    Alab = case_when(
+      A=="delta_TS" ~ "Change in telomere length between Years 1 and 2 (T/S ratio)",
+      A=="TS_t2" ~ "Telomere length at Year 1 (T/S ratio)",
+      A=="TS_t3" ~ "Telomere length at Year 2 (T/S ratio)"),
+    anthro = case_when(
+      grepl("laz",Y) ~ "LAZ",
+      grepl("waz",Y) ~ "WAZ",
+      grepl("wlz",Y) ~ "WLZ",
+      grepl("hcz",Y) ~ "HCZ",
+      grepl("len",Y) ~ "LEN",
+      grepl("wei",Y) ~ "WEIGHT",
+      grepl("hc_",Y) ~ "HCIR"
+    ),
+    anthro=factor(anthro,
+                  levels = c("LAZ","WAZ","WLZ","HCZ",
+                             "LEN","WEIGHT","HCIR"))) %>%
+  arrange(anthro) %>%
+  mutate(Ylab = factor(Ylab, levels=unique(Ylab)))
+
+head(d)
+
+
+#' some notes from our meeting:
+#'   -Figures 2 and 3 collapsed into 1 figure
+#' -Supplementary Figures updated (no density plots)
+#' -1 new supp figure: density plot comparing before and after regression to the mean.
+#' Thanks!
+
+yrange <- c(-0.4,0.5)
+ylabel="Adjusted difference in mean anthropometry Z-score\nbetween 25th and 75th percentile of Telomere measure"
+plotdf <- d %>% filter(H!=3, grepl("laz",Y)| grepl("waz",Y)| grepl("wlz",Y)| grepl("hcz",Y), !grepl("_Z",A))
+plotdf <- plotdf %>% mutate(
+  group = case_when(
+    grepl("delta_",Y) & grepl("_t2",A) ~ "Change in anthropometry\nTelomere length at year 1",
+    grepl("_t3",Y) & grepl("_t2",A) ~ "Anthropometry at year 2\nTelomere length at year 1",
+    grepl("_t2",Y) & grepl("_t2",A) ~ "Anthropometry and Telomere\nlength measured at year 1",
+    grepl("_t3",Y) & grepl("_t3",A) ~ "Anthropometry and Telomere\nlength measured at year 2"
+  )
+)
+
+p <- ggplot(plotdf, aes(x=anthro, y=point.diff)) + 
+  geom_pointrange(aes(ymin=lb.diff , ymax=ub.diff, color=anthro, group=anthro), #, group=Time, shape=Time),
+                  position = position_dodge(width = 0.4),
+                  size = 1) +
+  #geom_text(aes(label=ref), position = position_nudge(y = (abs(yrange[1])+abs(yrange[2]))/10)) +
+  facet_grid(~group) +
+  labs(y = ylabel, x =  "Anthropometry measure") +
+  geom_hline(yintercept = 0) +
+  coord_cartesian(ylim=yrange) +
+  scale_shape_manual(values=c(16,21)) +
+  scale_colour_manual(values=tableau10) + 
+  ggtitle("Timing of exposure measurement") +
+  theme_ki() +
+  theme(plot.title = element_text(hjust = 0),
+        panel.spacing = unit(0, "lines"),
+        legend.position = "right")+
+  guides(color = "none")
+p
+
+
+# pH1 <- tmle_plot_fun(d, 1, title="", ylabel="Difference in\nchange in Z-score")
+# pH2 <- tmle_plot_fun(d, 2, yrange=c(-0.065, 0.03), title="", cols=tableau10[c(5:7)],  ylabel="Difference in\nvelocity in cm or kg")
+# pH3 <- tmle_plot_fun(d, 3, title="", ylabel="Difference in mean Z-score")
+# pH5 <- tmle_plot_fun(d, 5, title="", ylabel="Difference in mean Z-score")
+# pH7 <- tmle_plot_fun(d, 7, yrange=c(-0.03, 0.03), title="", cols=tableau10[c(5:7)], ylabel="Difference in\nvelocity in cm or kg")
+# pH8 <- tmle_plot_fun(d, 8, title="", ylabel="Difference in\nchange in Z-score")
+
+
+#ggsave(p, file = here("figures/telo-growth-quartiles-differences_supp5.tiff"), height=6, width=14)
---
+++ table scripts/example-prescreening-table.R
@@ -0,0 +1,208 @@
+#example prescreening table:
+
+rm(list=ls())
+#renv::deactivate()
+source(here::here("0-config.R"))
+
+load(paste0(dropboxDir, "Data/Cleaned/Audrie/bangladesh-dm-ee-telo-growth-covariates-telolab-anthro.RData"))
+# Z-score telomere length
+d <- d %>%
+  mutate(TS_t2_Z = scale(TS_t2, center=T, scale=T)[,1]) %>%
+  mutate(TS_t3_Z = scale(TS_t3, center=T, scale=T)[,1]) %>%
+  mutate(delta_TS_Z = scale(delta_TS, center=T, scale=T)[,1])
+
+
+#clean covariates
+for(i in 1:ncol(d)){
+  cat(colnames(d)[i],"  ",class(d[,i]),"\n")
+}
+#set variables as factors/numeric
+d$sex<-as.factor(d$sex)
+d$sex <- factor(d$sex, labels = c("female", "male"))
+d$birthord<-as.factor(d$birthord)
+d$momage<-as.numeric(d$momage)
+d$momheight<-as.numeric(d$momheight)
+d$momedu<-as.factor(d$momedu)
+d$hfiacat<-as.factor(d$hfiacat)
+d$Nlt18<-as.numeric(d$Nlt18)
+d$Ncomp<-as.numeric(d$Ncomp)
+d$watmin<-as.numeric(d$watmin)
+d$floor<-as.factor(d$floor)
+d$walls<-as.factor(d$walls)
+d$elec<-as.factor(d$elec)
+d$asset_wardrobe<-as.factor(d$asset_wardrobe)
+d$asset_table<-as.factor(d$asset_table)
+d$asset_chair<-as.factor(d$asset_chair)
+d$asset_clock<-as.factor(d$asset_clock)
+d$asset_khat<-as.factor(d$asset_khat)
+d$asset_chouki<-as.factor(d$asset_chouki)
+d$asset_radio<-as.factor(d$asset_radio)
+d$asset_tv<-as.factor(d$asset_tv)
+d$asset_refrig<-as.factor(d$asset_refrig)
+d$asset_bike<-as.factor(d$asset_bike)
+d$asset_moto<-as.factor(d$asset_moto)
+d$asset_sewmach<-as.factor(d$asset_sewmach)
+d$asset_mobile<-as.factor(d$asset_mobile)
+d$n_cattle<-as.numeric(d$n_cattle)
+d$n_goat<-as.numeric(d$n_goat)
+d$n_chicken<-as.numeric(d$n_chicken)
+
+d$lenhei_med_t2<-as.numeric(d$lenhei_med_t2)
+d$weight_med_t2<-as.numeric(d$weight_med_t2)
+
+d$monsoon_ht2<-as.factor(d$monsoon_ht2)
+d$monsoon_ht2<-addNA(d$monsoon_ht2)
+levels(d$monsoon_ht2)[length(levels(d$monsoon_ht2))]<-"Missing"
+
+d$monsoon_ht3<-as.factor(d$monsoon_ht3)
+d$monsoon_ht3<-addNA(d$monsoon_ht3)
+levels(d$monsoon_ht3)[length(levels(d$monsoon_ht3))]<-"Missing"
+
+d$ageday_ht2<-as.numeric(d$ageday_ht2)
+d$ageday_ht3<-as.numeric(d$ageday_ht3)
+
+d$anthro_days_btwn_t2_t3<-as.numeric(d$anthro_days_btwn_t2_t3)
+
+d$tr <- factor(d$tr,levels=c("Control","Nutrition + WSH"))
+
+d$cesd_sum_t2<-as.numeric(d$cesd_sum_t2)
+d$cesd_sum_ee_t3<-as.numeric(d$cesd_sum_ee_t3)
+d$pss_sum_mom_t3<-as.numeric(d$pss_sum_mom_t3)
+
+d$diar7d_t2<-as.factor(d$diar7d_t2)
+d$diar7d_t2<-addNA(d$diar7d_t2)
+levels(d$diar7d_t2)[length(levels(d$diar7d_t2))]<-"Missing"
+
+d$diar7d_t3<-as.factor(d$diar7d_t3)
+d$diar7d_t3<-addNA(d$diar7d_t3)
+levels(d$diar7d_t3)[length(levels(d$diar7d_t3))]<-"Missing"
+
+d$life_viol_any_t3<-as.factor(d$life_viol_any_t3)
+d$life_viol_any_t3<-addNA(d$life_viol_any_t3)
+levels(d$life_viol_any_t3)[length(levels(d$life_viol_any_t3))]<-"Missing"
+
+
+
+
+#Loop over exposure-outcome pairs
+
+#### Association between telomere length at year 1 and child growth ####
+X <- c("TS_t2")            
+Y <- c("laz_t2") 
+
+W<-c("sex","birthord", "momage", "momheight","momedu", 
+             "hfiacat", "Nlt18", "Ncomp", "watmin", "floor", 
+             "walls", "elec", "asset_wardrobe", "asset_table", 
+             "asset_chair", "asset_clock", "asset_khat", 
+             "asset_chouki", "asset_radio", "asset_tv", "asset_refrig",
+             "asset_bike", "asset_moto", "asset_sewmach",
+             "asset_mobile", "n_cattle", "n_goat", "n_chicken",
+             "monsoon_ht2", "ageday_ht2", "tr", "cesd_sum_t2", "diar7d_t2", 
+             "life_viol_any_t3")
+
+
+forcedW = NULL
+V = NULL
+id = "clusterid"
+family = "gaussian"
+pval = 0.2
+
+  if (!is.null(W)) {
+    W <- subset(d, select = W)
+  }
+  Y <- subset(d, select = Y)
+  colnames(Y) <- "Y"
+  X <- subset(d, select = X)
+  colnames(X) <- "X"
+  id <- subset(d, select = id)
+  colnames(id) <- "id"
+  if (!is.null(V)) {
+    Vvar <- subset(d, select = V)
+    colnames(Vvar) <- "V"
+  }else{
+    Vvar <- data.frame(V = rep(1, nrow(d)))
+  }
+  if(!is.null(W)) {
+    gamdat <- data.frame(Y, X, id, Vvar, W)
+  }else{
+    gamdat <- data.frame(Y, X, id, Vvar)
+  }
+  if(!is.null(W)) {
+    if(sum(is.na(forcedW)) != 0) {
+      colnamesW <- names(W)
+    }
+    else {
+      if (is.null(forcedW)) {
+        Wnames <- names(W)
+        forcedW <- c(Wnames[Wnames == "tr" | grepl("age_", 
+                                                   Wnames) | grepl("agedays_", Wnames) | 
+                              grepl("ageday_", Wnames)])
+      }
+      cat("\nNon-prescreened covariates: ", paste(forcedW, 
+                                                  sep = "", collapse = ", "), "\n")
+      colnamesW <- names(W)[!(names(W) %in% forcedW)]
+    }
+    screenW <- subset(gamdat, select = colnamesW)
+  }else {
+    screenW <- NULL
+  }
+
+  
+  
+suppressWarnings(Wscreen <- washb_prescreen(Y = gamdat$Y, Ws = screenW, family = family, pval = pval, print = T))
+    
+Y = gamdat$Y
+Ws = screenW
+family = "gaussian"
+pval = 0.2
+print = TRUE
+    
+      require(lmtest)
+      if (family[[1]] == "neg.binom") {
+        require(MASS)
+      }
+      if (pval > 1 | pval < 0) {
+        stop("P-value threshold not set between 0 and 1.")
+      }
+      Ws <- as.data.frame(Ws)
+      dat <- data.frame(Ws, Y)
+      dat <- dat[complete.cases(dat), ]
+      nW <- ncol(Ws)
+      LRp <- matrix(rep(NA, nW), nrow = nW, ncol = 1)
+      rownames(LRp) <- names(Ws)
+      colnames(LRp) <- "P-value"
+      if (family[[1]] != "neg.binom") {
+        for (i in 1:nW) {
+          dat$W <- dat[, i]
+          if (class(dat$W) == "factor" & dim(table(dat$W)) == 
+              1) {
+            fit1 <- fit0 <- glm(Y ~ 1, data = dat, family = family)
+          }
+          else {
+            fit1 <- glm(Y ~ W, data = dat, family = family)
+            fit0 <- glm(Y ~ 1, data = dat, family = family)
+          }
+          LRp[i] <- lrtest(fit1, fit0)[2, 5]
+        }
+      }
+    
+      
+      
+      p20 <- ifelse(LRp < pval, 1, 0)
+        cat("\nLikelihood Ratio Test P-values:\n")
+        print(round(LRp, 5))
+          LRps <- matrix(LRp[p20 == 1, ], ncol = 1)
+          rownames(LRps) <- names(Ws)[p20 == 1]
+          colnames(LRps) <- "P-value"
+
+          
+  tab <- data.frame(Covariate=rownames(LRp), pval=as.vector(LRp), Selected=as.vector(ifelse(LRp<0.2,"Yes","No")))     
+  tab <- tab %>% arrange(pval) %>% mutate(pval=as.character(round(pval,3)))
+  tab$pval <- gsub("0.000", "<0.001", tab$pval)
+  colnames(tab)[2] <-"P-value"       
+  tab        
+          
+  knitr::kable(tab, format="pipe")      
+          
+          
+   
